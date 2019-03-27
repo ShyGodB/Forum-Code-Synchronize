6f6d3334434025bb052851716a4ed049cb24e83e
shygodb <qibingfang@gmail.com>
2019-03-27 11:06:39 +0800

    Upload project code

---
+++ app.js
@@ -0,0 +1,10 @@
+const Koa = require('koa');  // 引包
+const app = new Koa();  // 实例化
+
+app.use(async (ctx) => {  //async表示异步  ctx===context---上下文
+	ctx.body = "Hello World";   // ctx.body === 页面内容，这里就是指Hello World
+});
+
+app.listen(3000, () => {  //监听本地端口3000
+	console.log('Server is running at http://127.0.0.1:3000');
+});
